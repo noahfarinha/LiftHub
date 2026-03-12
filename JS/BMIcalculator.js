8d5e90ea9e7769823b4630dd9d25350365da5592
noahfarinha <noe-rendeiro@hotmail.com>
2026-03-12 19:22:03 +0000

    bmi update

--- JS/BMIcalculator.js
+++
@@ -1,9 +0,0 @@
-
-
-function bmiCalculator(weight, height) {
-    var bmi = weight / Math.pow(height, 2);
-    return Math.round(bmi);
-}
-
-var bmi = bmiCalculator(65, 1.80);
-console.log("Your BMI is: " + bmi);
